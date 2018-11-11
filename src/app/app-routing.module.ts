a44205d5e17054f60096ac65583e11b862a8e7c7
Hector Aguero <heagueron@gmail.com>
2018-11-11 07:51:21 -0400

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+//Modulo dedicado a las rutas
+
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from 'src/app/auth/register/register.component';
+import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { dashboardRoutes } from 'src/app/dashboard/dashboard.routes';
+
+const routes: Routes = [
+
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
+
+    { 
+        path: '', 
+        component: DashboardComponent,
+        children: dashboardRoutes   //children receives an array of routes
+        
+    },
+
+    { path: '**', redirectTo: ''}
+
+];
+
+@NgModule({
+
+    imports: [
+        RouterModule.forRoot( routes )
+    ],
+    exports: [
+        RouterModule
+    ]
+
+})
+
+export class AppRoutingModule {}
---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from "@angular/router";
+import { EstadisticaComponent } from "src/app/ingreso-egreso/estadistica/estadistica.component";
+import { IngresoEgresoComponent } from "src/app/ingreso-egreso/ingreso-egreso.component";
+import { DetalleComponent } from "src/app/ingreso-egreso/detalle/detalle.component";
+
+//Routes inside Dashboard component
+
+
+
+export const dashboardRoutes: Routes = [
+
+    { path: '', component: EstadisticaComponent},
+    { path: 'ingreso-egreso', component: IngresoEgresoComponent},
+    { path: 'detalle', component: DetalleComponent}
+
+]
